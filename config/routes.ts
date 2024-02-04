68330b72d05a1aaa8e91fab09ef66083cdf8af5d
RY's <medayb45esi@gmail.com>
2024-02-04 14:52:00 +0100

    UI: start implementing  header

---
+++ config/routes.ts
@@ -0,0 +1,10 @@
+import path from 'path';
+
+const availableRoutes = [
+	{ path: '/', title: 'Home page', desc: 'This is Home page' },
+	{
+		path: '/chapter',
+		title: 'Chapter',
+		desc: 'This is Chapter page',
+	},
+];
