2b9a37345f40acd3e1bc65590f592d6f582d2bf7
merzouka <marzoukayouness@gmail.com>
2024-02-04 15:51:25 +0100

    removed test code

--- chapter5/interpolate.r
+++ chapter5/interpolate.r
@@ -6,9 +6,8 @@ points <- function(...) {
   points <- points[order(sapply(points, `[`, 1))]
 }
 
-poly_multiply <- function(p, q) {
+mul <- function(p, q) {
   m <- matrix(p, ncol = 1) %*% q
-  print(m)
   result <- c(NULL)
   for (i in seq_len(nrow(m))) {
     for (j in seq_len(ncol(m))) {
@@ -19,10 +18,29 @@ poly_multiply <- function(p, q) {
       result[i + j - 1] <- result[i + j - 1] + m[i, j]
     }
   }
+  return(c(result))
+}
+
+sum <- function(p, q) {
+  if (length(p) == length(q)) {
+    return(p + q)
+  }
+
+  if (length(p) > length(q)) {
+    bigger <- p
+    smaller <- q
+  } else {
+    bigger <- q
+    smaller <- p
+  }
+
+  result <- bigger[seq(1, length(smaller))] + smaller
+  result <- c(result, bigger[seq(length(smaller) + 1, length(bigger))])
+
   return(result)
 }
 
-poly_print <- function(coefficients) {
+stringify <- function(coefficients) {
   i <- 0
   coefficients <- Filter(
     function(x) return(x != ""),
@@ -46,19 +64,83 @@ poly_print <- function(coefficients) {
       }
     )
   )
-  print(paste(coefficients, collapse = " + "))
+  return(paste(coefficients, collapse = " + "))
+}
+
+multiply <- function(...) {
+  result <- c(1)
+  for (i in seq_len(...length())) {
+    result <- mul(result, ...elt(i))
+  }
+  return(result)
 }
 
 lagrange <- function(...) {
+  print("                                   Lagrange Method")
+  p <- points(...)
+  result <- c(NULL)
+  for (i in seq_len(length(p))) {
+    weight <- c(1)
+    for (j in seq_len(length(p))) {
+      if (j != i) {
+        weight <- mul(weight, (1 / (p[[i]][1] - p[[j]][1])) * c(- p[[j]][1], 1))
+      }
+    }
+    print(sprintf("l%g(x) = %s", i, stringify(weight)))
+    result <- sum(result, weight * p[[i]][2])
+  }
+  return(result)
+}
 
+newton <- function(...) {
+  p <- points(...)
+  execution <- matrix(c(sapply(p, `[`, 2)), nrow = length(p))
+  header <- c("x", "f[x0]")
+  result <- p[[1]][2]
+  # first 2 column are already filled
+  for (i in seq(2, length(p))) {
+    header <- c(
+      header,
+      sprintf(
+        "f[%s]",
+        paste(
+          sapply(
+            seq(0, i - 1),
+            function(i) return(sprintf("x%g", i))
+          ),
+          collapse = ", "
+        )
+      )
+    )
+
+    # starts calculating at position [2, 2] before should be empty
+    column <- rep(0, i - 1)
+    for (j in seq(i, length(p))) {
+      cell <- (execution[j, i - 1] - execution[j - 1, i - 1]) /
+        (p[[j]][1] - p[[j - (i - 1)]][1])
+      if (j == i) {
+        result <- c(result, cell)
+      }
+
+      column <- c(
+        column,
+        cell
+      )
+    }
+    execution <- cbind(execution, column)
+  }
+
+  print("                                Newton Algorithm")
+  execution <- cbind(sapply(p, `[`, 1), execution)
+  execution <- rbind(header, execution)
+  print(execution)
+  return(result)
 }
 
 divided_diff <- function(...) {
-
 }
 
 finite_diff <- function(...) {
-
 }
 
 interpolate <- function(...) {
@@ -70,5 +152,3 @@ interpolate <- function(...) {
   print(result)
   return(result)
 }
-
-poly_print(c(1, 1, 2))
