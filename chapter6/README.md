dba1a756d060739e915f924122d06dbd2c6c83ae
merzouka <marzoukayouness@gmail.com>
2024-02-05 11:45:46 +0100

    added readme, changed support to jpeg, added requirments

---
+++ chapter6/README.md
@@ -0,0 +1,27 @@
+## Setup
+- install `python<version>`
+- install `pip`
+- install virtualenv using `pip install virtualenv`
+- create a virtual env using `python<version> venv -m [folder name]`
+- execute `[folder name]/bin/activate` script (use `source [folder name]/bin/activate` on Linux)
+- run pip install -r requirements.txt
+- run the script using `python blur.py`
+
+follow (https://www.freecodecamp.org/news/how-to-setup-virtual-environments-in-python/)[freecodecamp article] if 
+you get stuck in the dependency installation process
+
+### NOTE
+make sure you execute the script, after entering into the virtual environment
+
+## Explanation
+The given program blurs an image using gaussian blur technique.
+For every pixel of the original image a new pixel is generated using the surrounding pixel values in a 
+`5*5` matrix. The contribution of each pixel into the output is determined using a weight. Weights are 
+stored in a `5*5` matrix called the __kernel__, and are determined using the integral of the 
+normal density function (normal distribution). Since the former function doesn't have a definite primitive 
+numerical method are used to compute the integral in intervals of length __(b - a) / 25__ where b = 2 
+and a = -2 ([-2, -1.84], [-1.84, -1.68], ...). For composite methods 3 sub intervals are used per integral. 
+After computation the obtained values (`Xi`) are normalized by dividing them by their sum 
+__Xi / sum(Xi)__ the output is then stored in the __kernel__. The new pixel value is obtained by summing 
+the product of surrounding pixels by their respective weights (pixel[1, 1] * kernel[1, 1]).
+
--- chapter6/blur.py
+++ chapter6/blur.py
@@ -3,20 +3,31 @@ import matplotlib.pyplot as plt
 import os
 from multiprocessing import Pool
 
-img = plt.imread("./sample.png")
+path = input("image path(.jpg)[Enter to use sample.jpg]: ")
+while path != "" and not os.path.exists(path):
+    print("invalid path")
+    path = input("image path(.jpg)[Enter to use sample.jpg]: ")
+
+path = path or "sample.jpg"
+
+sigma = -1
+while sigma > 2 or sigma < 0:
+    sigma = float(input("σ [0, 2](higher sigma mean higher distortion): "))
+
+img = plt.imread(f"{path}")
 kernels = dict()
 channels = len(img[0, 0])
 
 def kernel(mode):
-    output = os.popen(f"Rscript kernel.r {mode} 1").read()
+    output = os.popen(f"Rscript kernel.r {mode} {sigma}").read()
     kernels[mode] = np.array(
         list(
             map(
-                lambda weight: [weight if i < 3 else 1 for i in range(4)],
+                lambda weight: [weight if i < 3 else 1 for i in range(channels)],
                 list(map(lambda x: float(x) - 0.0000001, output[output.index('"'):-1].strip('"').split(";")))
             )
         )
-    ).reshape(5, 5, 4)
+    ).reshape(5, 5, channels)
 
 modes = ["trapezoids", "simpson", "simple_trapezoids", "simple_simpson"]
 
@@ -26,7 +37,7 @@ for mode in modes:
 def blur(mode):
     kernel = kernels[mode]
     print(f"processing: {mode}")
-    img_new = np.empty(img.shape)
+    img_new = np.empty(img.shape, dtype=np.uint8)
     for pixrow in range(0, img.shape[0]):
         for pixcol in range(0, img.shape[1]):
             left_add = max(-2, 0 - pixcol)
@@ -39,14 +50,14 @@ def blur(mode):
                     kernel[(2 + top_add):(2 + bottom_add), (2 + left_add):(2 + right_add),],
                     pixels,
                 ),
-                axis = (0, 1)
+                axis = (0, 1),
             )
             if (len(img_new[pixrow, pixcol]) > 3):
                 img_new[pixrow, pixcol, 3] = min(
-                    img_new[pixrow, pixcol, 3] / (np.sum(pixels, axis = (0, 1))[3] or 1),
-                    0.999999
+                    int(img_new[pixrow, pixcol, 3] / (np.sum(pixels, axis = (0, 1))[3] or 1)),
+                    255
                 )
 
-    plt.imsave(f"./output-{mode}.png", img_new)
+    plt.imsave(f"./output-{mode}.jpg", img_new)
 
 result = Pool(processes = len(modes)).map(blur, modes)
