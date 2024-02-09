44810857a1bf540d373ae1496e915d59eef267da
merzouka <marzoukayouness@gmail.com>
2024-02-10 00:02:32 +0100

    fixed read me

--- server/src/chapter6/console.r
+++ server/src/chapter6/console.r
@@ -2,11 +2,16 @@
 args <- commandArgs(trailingOnly = TRUE)
 
 # Rscript console.r [image path] sigma
-sigma <- as.numeric(args[2])
-if (sigma < 0 || sigma > 2) {
-  stop("invalid sigma value")
+if (length(args) == 2) {
+  sigma <- as.numeric(args[2])
+  if (sigma < 0 || sigma > 2) {
+    stop("invalid sigma value")
+  }
+  image <- magick::image_read(args[1])
+} else {
+  sigma <- 1
 }
-image <- magick::image_read(args[1])
+
 
 source(sprintf("%s/blur.r", getwd()))
 blur(image, sigma)
