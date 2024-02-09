aa06e092116c34b9249f78c56f8b38c83c762598
merzouka <marzoukayouness@gmail.com>
2024-02-09 23:25:27 +0100

    changed read me, added some validation

--- server/src/chapter6/console.r
+++ server/src/chapter6/console.r
@@ -3,6 +3,9 @@ args <- commandArgs(trailingOnly = TRUE)
 
 # Rscript console.r [image path] sigma
 sigma <- as.numeric(args[2])
+if (sigma < 0 || sigma > 2) {
+  stop("invalid sigma value")
+}
 image <- magick::image_read(args[1])
 
 source(sprintf("%s/blur.r", getwd()))
